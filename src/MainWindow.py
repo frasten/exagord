e4eb3a0f3a6f21ec2073e80ec8dc0228cbadfb34
Andrea Piccinelli <frasten@gmail.com>
2010-01-17 18:05:08 +0100

    Renamed some variables
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -4,7 +4,7 @@ import gtk
 
 
 class MainWindow:
-	SIZE = 800, 600
+	width, height = 800, 600
 
 	def delete_event(self, widget, event, data=None):
 		print "delete event occurred"
@@ -13,28 +13,27 @@ class MainWindow:
 	def __init__(self):
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("destroy", self.destroy)
-		self.window.set_default_size(self.SIZE[0], self.SIZE[1])
+		self.window.set_default_size(self.width, self.height)
 		self.window.set_title("Exagord")
 		
 		# Sets the border width of the window.
 		self.window.set_border_width(10)
 
-		self.window.move((gtk.gdk.screen_width() - self.SIZE[0]) / 2,
-		  (gtk.gdk.screen_height() - self.SIZE[1]) / 2)
+		self.window.move((gtk.gdk.screen_width() - self.width) / 2,
+		  (gtk.gdk.screen_height() - self.height) / 2)
 
-		# Contenitore
-		self.hbox = gtk.HBox()
+		# Main Container
+		self.mainbox = gtk.HBox()
 		button = gtk.Button("PanelL")
-		self.hbox.pack_start(button, False, False, 0)
+		self.mainbox.pack_start(button, False, False, 0)
 		button2 = gtk.Button("HarmonicTable")
-		self.hbox.pack_start(button2, True, False, 0)
+		self.mainbox.pack_start(button2, True, False, 0)
 		button3 = gtk.Button("PanelR")
-		self.hbox.pack_start(button3, False, False, 0)
+		self.mainbox.pack_start(button3, False, False, 0)
 
 
-
-		self.window.add(self.hbox)
-		self.hbox.show_all()
+		self.window.add(self.mainbox)
+		self.mainbox.show_all()
 
 		# show the main window
 		self.window.show()
