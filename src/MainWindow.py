69a0763b9cb9612728c6c56ec4f8a5c50136d49c
Andrea Piccinelli <frasten@gmail.com>
2010-01-17 19:21:33 +0100

    Added a central top bar.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -27,14 +27,23 @@ class MainWindow:
 
 		# LEFT PANEL
 		panelL = gtk.Frame("Left Panel")
+
+		# Elements buttons
 		buttonBox = gtk.VButtonBox()
 		buttonBox.set_border_width(5)
 
 		buttonBox.set_layout(gtk.BUTTONBOX_START)
 		buttonBox.set_spacing(20)
 
-		button1 = gtk.Button(stock=gtk.STOCK_OK)
+		button1 = gtk.Button()
+		# Queste n istruzioni serviranno quando mostreremo solo l'immaginetta
+		button1.set_relief(gtk.RELIEF_NONE)  # Elimino il bordo di default
+		button1.set_focus_on_click(False)
+		button1.set_tooltip_text("First Element")
+		icon = gtk.image_new_from_stock(gtk.STOCK_CLOSE, gtk.ICON_SIZE_MENU)
+		button1.add(icon)  # Metto un'immagine al button
 		buttonBox.add(button1)
+
 		button2 = gtk.Button(stock=gtk.STOCK_OK)
 		buttonBox.add(button2)
 		button3 = gtk.Button(stock=gtk.STOCK_OK)
@@ -51,6 +60,44 @@ class MainWindow:
 		# CENTRAL PANEL
 		centralPanel = gtk.Frame("Central Panel")
 		self.mainbox.pack_start(centralPanel, True, True, 0)
+
+		# Split into two:
+		centralSplitBox = gtk.VBox()
+
+
+		# TopBar
+		centralTopbar = gtk.HBox()
+		
+		# **** TPM **** 
+		# value, min, max, increment, pageIncrement, boh?
+		centralTopbar.pack_start(gtk.Label("TPM "), False, False, 0)
+		tpmAdj = gtk.Adjustment(60, 1, 300, 1, 10, 0.0)
+		tpm = gtk.SpinButton(tpmAdj, 0, 0)
+		tpm.set_wrap(True)
+		centralTopbar.pack_start(tpm, False, True, 0)
+
+		# **** Octave **** 
+		centralTopbar.pack_start(gtk.Label("Octave "), False, False, 0)
+		octaveAdj = gtk.Adjustment(0, -8, 8, 1, 2, 0.0)
+		octave = gtk.SpinButton(octaveAdj, 0, 0)
+		octave.set_wrap(True) # TODO: ???
+		centralTopbar.pack_start(octave, False, True, 0)
+
+		# **** Midi Ch. **** 
+		centralTopbar.pack_start(gtk.Label("Midi Ch. "), False, False, 0)
+		midiChAdj = gtk.Adjustment(0, -8, 8, 1, 2, 0.0)
+		midiCh = gtk.SpinButton(midiChAdj, 0, 0)
+		midiCh.set_wrap(True) # TODO: ???
+		centralTopbar.pack_start(midiCh, False, True, 0)
+
+
+		# Aggiungo la topbar al pannello centrale
+		centralSplitBox.pack_start(centralTopbar, False, False, 0)
+
+		# Harmonic Table
+		centralSplitBox.pack_start(gtk.Button(), True, True, 0)
+		centralPanel.add(centralSplitBox)
+
 
 
 		# RIGHT PANEL
